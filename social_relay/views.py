b5cc566967e531a28d1b52a0ada2514395fa5f91
Jason Robinson <mail@jasonrobinson.me>
2015-07-15 23:39:49 +0300

    Remove the temporary posts stuff
    Just save any receive_public to redis for later opening

--- social_relay/views.py
+++ social_relay/views.py
@@ -1,11 +1,8 @@
 import json
-from random import random
 import redis
 
-from flask import render_template, request, flash, url_for, redirect, Response, abort
+from flask import render_template, request, Response, abort
 
-from federation.controllers import handle_receive, handle_create_payload
-from federation.entities.base import Post
 from federation.hostmeta.generators import generate_host_meta, generate_legacy_webfinger, generate_hcard
 
 from social_relay import app
@@ -15,13 +12,9 @@ r = redis.Redis(host=app.config.get("REDIS_HOST"), port=app.config.get("REDIS_PO
 
 
 @app.route('/')
-def show_posts():
-    idlist = r.lrange('idlist', 0, -1)
-    posts = []
-    for i in idlist:
-        posts.append({"handle": r.get('posts:%s:handle' % i.decode("utf-8")).decode("utf-8"),
-                      "raw_content": r.get('posts:%s:raw_content' % i.decode("utf-8")).decode("utf-8")})
-    return render_template('show_posts.html', idlist=idlist, posts=posts)
+def show_public_queue():
+    items = r.lrange("receive_public", 0, 29)
+    return render_template('show_public_queue.html', items=items)
 
 
 @app.route('/.well-known/host-meta')
@@ -66,17 +59,8 @@ def hcard(guid):
 @app.route("/receive/public", methods=["POST"])
 def receive_public():
     payload = request.form["xml"]
-    sender, protocol_name, entities = handle_receive(payload, skip_author_verification=True)
-
-    for entity in entities:
-        print(entity)
-        # We only care about posts atm
-        if isinstance(entity, Post):
-            id = r.incr('global:nextPostId')
-            r.lpush('idlist', id)
-            r.set('posts:%s:raw_content' % id, '%s' % entity.raw_content)
-            r.set('posts:%s:guid' % id, '%s' % entity.guid)
-            r.set('posts:%s:handle' % id, '%s' % entity.handle)
+    # Push to redis for later inspection
+    r.lpush("receive_public", payload)
 
     # return 200 whatever
     data = {
@@ -84,19 +68,3 @@ def receive_public():
     }
     js = json.dumps(data)
     return Response(js, status=200, mimetype='application/json')
-
-
-@app.route('/add')
-def add_entry():
-    raw_content = str(random())
-    guid = str(random())
-    handle = "%s@%s.%s" % (str(random()), str(random()), str(random()))
-    id = r.incr('global:nextPostId')
-    r.lpush('idlist', id)
-    r.set('posts:%s:raw_content' % id, '%s' % raw_content)
-    r.set('posts:%s:guid' % id, '%s' % guid)
-    r.set('posts:%s:handle' % id, '%s' % handle)
-    entity = Post(raw_content=raw_content, guid=guid, handle=handle)
-    # data = handle_create_payload()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_posts'))

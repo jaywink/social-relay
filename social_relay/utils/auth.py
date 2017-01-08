7f6341cde7c5f0cdb513255fbfd68fe54d15f21a
Jason Robinson <mail@jasonrobinson.me>
2017-01-08 22:52:25 +0200

    Fix PEP8 issue in auth.py

--- social_relay/utils/auth.py
+++ social_relay/utils/auth.py
@@ -6,7 +6,7 @@ from social_relay import app
 def check_auth(username, password):
     """This function is called to check if a username password combination is valid."""
     return username == app.config.get("RQ_DASHBOARD_USERNAME") and \
-           password == app.config.get("RQ_DASHBOARD_PASSWORD")
+        password == app.config.get("RQ_DASHBOARD_PASSWORD")
 
 
 def authenticate():

3f541ffdce717fd3441d7c30396658c6222c5bfb
Jason Robinson <mail@jasonrobinson.me>
2017-01-10 22:23:36 +0200

    Add circus to run tasks and workers
    Closes #49

--- tasks/schedule_jobs.py
+++ tasks/schedule_jobs.py
@@ -3,6 +3,7 @@ import time
 
 
 if __name__ == '__main__':
+    print("Scheduling all jobs")
     from tasks.fetch_pod_list import schedule_job
     schedule_job()
     from tasks.poll_pods import schedule_job
